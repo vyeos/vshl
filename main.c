fbd034f1cae198b360ba8c7fc35394ddf60a4df4
vyeos <rudranpatel0980@gmail.com>
2026-01-18 22:47:47 +0530

    fix: better loop

--- main.c
+++ main.c
@@ -1,17 +1,36 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h> // mem alloc
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h> // fork, exec, pid_t
 
 int main() {
-  bool end = false;
-  char input[1024];
+  char *line = NULL;
+  size_t len = 0; // initial len -> will increase while typing
+  ssize_t nread;
 
-  while (!end) {
+  while (true) {
     printf("myshell> ");
-    scanf("%s", input);
-    if (strcmp(input, "exit") == 0) {
-      end = true;
+    fflush(stdout);
+    nread = getline(&line, &len, stdin);
+
+    if (nread == -1) { // checking for \0
+      printf("\nExiting...\n");
+      break;
     }
+
+    if (nread > 0 && line[nread - 1] == '\n') {
+      line[nread - 1] = '\0';
+    }
+
+    if (strcmp(line, "exit") == 0) {
+      break;
+    }
+
+    // write future code here
   }
+  free(line);
   return 0;
 }
